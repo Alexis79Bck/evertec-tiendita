1ec55543a4b6825f49d71d7ce3e8f6b3fdb06759
Alexis Mata WebDev <alexedmata@gmail.com>
2022-09-19 09:44:10 -0400

    #3.1 Correcciones

--- resources/views/step-1.blade.php
+++ resources/views/step-1.blade.php
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{ config('app.name') }}</title>
-
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-        <style>
-            body {
-                font-family: Verdana, Geneva, Tahoma, sans-serif sans-serif;
-            }
-        </style>
-    </head>
-    <body class="antialiased">
-       
-        <!-- CONTAINER -->
-        <div class="container d-flex align-items-center min-vh-100">
-            <div class="row g-0 justify-content-center">
-                <!-- TITLE -->
-                <div class="col-lg-4 offset-lg-1 mx-0 px-0">
-                    <div id="title-container">
-                        
-                        <h2>--- Order Request ---</h2>
-                       
-                    </div>
-                </div>
-                <!-- FORMS -->
-                <div class="col-lg-7 mx-0 px-0">
-                    <div class="progress">
-                        <div aria-valuemax="100" aria-valuemin="0" aria-valuenow="50"
-                            class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" style="width: 0%">
-                        </div>
-                    </div>
-                    <div id="qbox-container">
-                        <form class="needs-validation" id="form-wrapper" method="post" name="form-wrapper" novalidate>
-                            <!-- STEPS HERE -->
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-       
-
-        <script src="{{ asset('bootstrap.bundle.min.js') }}"></script>
-    </body>
-</html>
-=======
 @extends('layouts.app')
 
 
@@ -119,4 +69,3 @@
 
 
 
->>>>>>> 66cb4cdc8c87558ef38b76c735aa9779eba32761
