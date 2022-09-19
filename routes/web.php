8de4f822708e3d8ed3e5bccc73de4b1835a44ae7
Alexis Mata WebDev <alexedmata@gmail.com>
2022-09-19 09:41:39 -0400

    #3 Ordenando Commits

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,8 @@ use App\Http\Controllers\OrderController;
 |
 */
 
-<<<<<<< HEAD
-Route::get('/', function () {
-    return view('step-1');
-});
-=======
 Route::get('/', [PageController::class,'index'])->name('home');
 Route::get('/new-order', [OrderController::class, 'newOrder'])->name('newOrder');
 Route::get('/detail-order', [OrderController::class, 'detailOrder'])->name('step-2');
 Route::get('/proceed-order', [OrderController::class, 'proceedOrder'])->name('step-3');
 Route::post('/customer', [CustomerController::class, 'register'])->name('register');
->>>>>>> 66cb4cdc8c87558ef38b76c735aa9779eba32761
